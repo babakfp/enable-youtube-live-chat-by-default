d5ae38d9a05473a016dcfce46e8fbcacf01c7e91
babakfp <babak.bxf@gmail.com>
2023-09-16 09:41:19 -0700

    Work

--- src/content.js
+++ src/content.js
@@ -1,3 +1,4 @@
+// Check Chrome storage for the isEnabled flag.
 chrome.storage.local.get().then(({ isEnabled }) => {
     if (isEnabled) {
         switchToLiveChat()
@@ -5,26 +6,82 @@ chrome.storage.local.get().then(({ isEnabled }) => {
 })
 
 // TODO: Maybe add try/catch?
+// Listen for messages from the extension.
 chrome.runtime.onMessage.addListener(({ isEnabled }) => {
     if (isEnabled) {
         switchToLiveChat()
     }
 })
 
+// Function to switch to Live Chat.
 function switchToLiveChat() {
-    waitForQuerySelector("#chatframe", iframe => {
-        iframe.contentWindow.addEventListener("load", () => {
-            const triggerButton =
-                iframe.contentDocument.querySelector("#trigger")
-            if (!triggerButton) return
-            triggerButton.click()
-            const liveChatButton = iframe.contentDocument.querySelector(
-                '#menu > a[aria-selected="false"]',
-            )
-            if (!liveChatButton) return
-            liveChatButton.click()
-        })
+    /** @type {HTMLIFrameElement | null} */
+    const iframe = document.querySelector("#chatframe")
+    if (iframe) {
+        fireClickActions(iframe)
+    } else {
+        waitForQuerySelector(
+            "#chatframe",
+            /** @param {HTMLIFrameElement} iframe */ iframe => {
+                fireClickActions(iframe)
+            },
+        )
+    }
+}
+
+/**
+ * @param {HTMLIFrameElement} iframe
+ * @returns
+ */
+function fireClickActions(iframe) {
+    let didGoInsideLoad = false
+
+    // Add an event listener for the iframe's load event.
+    iframe.contentWindow.addEventListener("load", () => {
+        didGoInsideLoad = true
+        clickToSwitch(iframe)
     })
+
+    // If the load event didn't trigger (iframe already loaded), call clickToSwitch.
+    if (!didGoInsideLoad) {
+        clickToSwitch(iframe)
+    }
+}
+
+/**
+ * Clicks to switch to "Live chat replay" mode if not already on that mode.
+ * @param {Document} param0 - An object with a `contentDocument` property representing the document to interact with.
+ */
+function clickToSwitch({ contentDocument: document }) {
+    if (isAlreadyOnLiveChat(document)) return
+
+    const triggerButton = document.querySelector("#trigger")
+    if (!triggerButton) return
+    triggerButton.click()
+
+    const liveChatButton = document.querySelector("#menu > a:last-of-type")
+    if (!liveChatButton) return
+    liveChatButton.click()
+}
+
+/**
+ * Checks if the chat is already on "Live chat replay" mode.
+ * @param {Document} document - The document object.
+ * @returns {boolean} True if on "Live chat replay" mode, false otherwise.
+ */
+function isAlreadyOnLiveChat(document) {
+    return getDropdownButtonLabelTextContent(document) === "Live chat replay"
+}
+
+/**
+ * Get the text content of the chat filter dropdown button.
+ * @param {Document} document - The document object.
+ * @returns {"Top chat replay" | "Live chat replay"} The text content of the dropdown button.
+ */
+function getDropdownButtonLabelTextContent(document) {
+    /** @type {HTMLDivElement} */
+    const labelElement = document.querySelector("#label-text")
+    return labelElement.textContent
 }
 
 /**
--- src/popup.js
+++ src/popup.js
@@ -1,29 +1,54 @@
+/**
+ * Options for the extension.
+ * @typedef {object} Options
+ * @property {boolean} isEnabled - Indicates whether the extension is enabled or disabled.
+ */
+
+/** @type {HTMLInputElement} */
 const toggleInput = document.getElementById("isEnabled")
 
+// Load the extension state from extension local storage.
 chrome.storage.local.get().then(({ isEnabled }) => {
     if (isEnabled) {
         toggleInput.checked = true
     }
 })
 
+// Listen for changes to the toggle input.
 toggleInput.addEventListener("change", e => {
     const isChecked = e.target.checked
 
     ;(async () => {
         // TODO: Maybe add try/catch?
+        // Get the current active tab.
         const firstTab = await getCurrentTab()
+
+        // Create options object with the new state.
         const options = { isEnabled: isChecked }
-        await sendOptions(firstTab, options)
+
+        // Send options to the content script.
+        sendOptions(firstTab, options)
+
+        // Store the options in local storage.
         await chrome.storage.local.set(options)
     })()
 })
 
+/**
+ * Get the current active tab in the browser.
+ * @returns {Promise<chrome.tabs.Tab>} A Promise that resolves to the active tab.
+ */
 async function getCurrentTab() {
     const queryOptions = { active: true, currentWindow: true }
     const [firstTab] = await chrome.tabs.query(queryOptions)
     return firstTab
 }
 
-async function sendOptions(firstTab, options) {
-    await chrome.tabs.sendMessage(firstTab.id, options)
+/**
+ * Send options to a specified tab.
+ * @param {chrome.tabs.Tab} tab - The tab to send options to.
+ * @param {Options} options - The options to send.
+ */
+function sendOptions(tab, options) {
+    chrome.tabs.sendMessage(tab.id, options)
 }
