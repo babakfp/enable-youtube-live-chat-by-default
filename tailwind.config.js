c138d98f360ea74ee6c3f8f0161416b435ef39b7
babakfp <babak.bxf@gmail.com>
2023-09-16 13:35:48 -0700

    Updated colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+import colors from "tailwindcss/colors"
+
 /** @type {import("tailwindcss").Config} */
 export default {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            gray: colors.zinc,
+        },
     },
     plugins: [],
 }
